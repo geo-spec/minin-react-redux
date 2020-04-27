32eb89b6462c5b8be5be69534fdcfd713419dfe2
Georgy <shilingeorgiy@yandex.ru>
2020-04-28 01:40:05 +0300

    56:30

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,1 +1,4 @@
-export const CREATE_POST = 'POST/ REATE_POST'
+export const CREATE_POST = 'POST/CREATE_POST'
+export const FETCH_POSTS = 'POST/FETCH_POSTS'
+export const SHOW_LOADER = 'APP/FETCH_POSTS'
+export const HIDE_LOADER = 'APP/FETCH_POSTS'
