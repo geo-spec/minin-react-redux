37cd76410afa5963662c1f77111c92d9979b70ca
Georgy <shilingeorgiy@yandex.ru>
2020-05-03 20:20:49 +0300

    1:17:00

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -37,13 +37,58 @@ export function hideAlert(text) {
 }
 
 export function fetchPosts() {
+
+
+    // return async dispatch => {
+    //   try {
+    //     dispatch(showLoader())
+    //     const response = await fetch('ttps://jsonplaceholder.typicode.com/posts?_limit=5')
+    //     const json = await response.json()
+    //     setTimeout(() => {
+    //       dispatch({ type: FETCH_POSTS, payload: json })
+    //       dispatch(hideLoader())
+    //     }, 500)
+    //   } catch (e) {
+    //     dispatch(showAlert('Что-то пошло не так'))
+    //     dispatch(hideLoader())
+    //   }
+    // }
+
   return async dispatch => {
-    dispatch(showLoader())
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-    const json = await response.json()
-    dispatch({ type: FETCH_POSTS, payload: json })
-    dispatch(hideLoader())
+
+    try {
+      dispatch(showLoader())
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      const json = await response.json()
+      // пример искуственной задержки
+      setTimeout(() => {
+        dispatch({type: FETCH_POSTS, payload: json})
+        dispatch(hideLoader())
+
+      }, 500) // 5000 милисекунд
+    } catch (e) {
+        dispatch(showAlert('Some error'))
+        dispatch(hideLoader())
+    }
   }
+
+
 }
 
 
+
+// try {
+//   dispatch(showLoader())
+//   const response = await fetch('ttps://jsonplaceholder.typicode.com/posts?_limit=5')
+//   const json = await response.json()
+//   // пример искуственной задержки
+//   setTimeout(() => {
+//     dispatch({type: FETCH_POSTS, payload: json})
+//     dispatch(hideLoader())
+//
+//   }, 500) // 5000 милисекунд
+// } catch (e) {
+//   dispatch(showLoader('Some error'))
+//   dispatch(hideAlert())
+// }
+// }
